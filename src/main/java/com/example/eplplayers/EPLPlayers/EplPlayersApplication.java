8eb6c92fb6769fede4ea641f1039c688b3875a55
AnhDuc <ducleanh2005@gmail.com>
2025-08-01 22:06:42 -0500

    Initial commit

---
+++ src/main/java/com/example/eplplayers/EPLPlayers/EplPlayersApplication.java
@@ -0,0 +1,13 @@
+package com.example.eplplayers.EPLPlayers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EplPlayersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EplPlayersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eplplayers/EPLPlayers/EplPlayersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eplplayers.EPLPlayers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EplPlayersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
