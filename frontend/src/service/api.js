44c3b165ea88faf57e88025337963d3cc60050a9
AnhDuc <ducleanh2005@gmail.com>
2025-09-02 23:13:32 -0500

    fix env var import

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = process.env.BACKEND_URL
+const API_BASE_URL = import.meta.env.VITE_BACKEND_URL
+console.log("API_BASE_URL:", API_BASE_URL);
 
 export async function fetchPlayers({ team, position, nation, name }) {
     const params = new URLSearchParams();
