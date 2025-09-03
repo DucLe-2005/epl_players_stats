635b3bdccdab059bb6e214480dda8eef423e50dd
AnhDuc <ducleanh2005@gmail.com>
2025-09-02 22:37:30 -0500

    update backend url

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8080/api/v1/player"
+const API_BASE_URL = process.env.BACKEND_URL
 
 export async function fetchPlayers({ team, position, nation, name }) {
     const params = new URLSearchParams();
